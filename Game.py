a4216faea4b078aece3859b79f34a4fae76958cc
Aya <1220020@student.birzeit.edu>
2025-09-04 18:38:07 +0300

    add Utiles.py
     The Utiles.py have the screen and init setting for the game

---
+++ Game.py
@@ -0,0 +1,13 @@
+
+
+class Game:
+    
+    def __init__(self):
+        pass
+    
+    def update(self):
+        pass
+    
+    def draw(self):
+        pass 
+    
---
+++ Utils.py
@@ -0,0 +1,43 @@
+import pygame
+from pygame import DOUBLEBUF
+from Box2D import b2World
+
+
+class Utils():
+    
+    def __init__(self):
+        pygame.init()
+        self.width = 600
+        self.height = 600
+        self.screen = pygame.display.set_mode((self.width , self.height) , DOUBLEBUF , 16 )
+        self.dt =0 
+        self.clock = pygame.time.Clock()
+        
+        
+        self.PPM =10.0 # pixcles per meter
+        self.world = b2World(gravity=(0,-20) , doSleep=True)
+        
+        
+        
+        
+        
+    def to_Pos(self, pos):
+       """ Convert from Box2D to pygame coordintes"""
+       return (pos[0] * self.PPM , self.height -(pos[1] * self.PPM))
+   
+   
+    def from_Pos(self , pos):
+       """ Convert from pygame to Box2D coordintes"""
+       return (pos[0] / self.PPM , self.height -(pos[1] / self.PPM))
+    
+    
+    def calDeltaTime(self):
+        t =self.clock.tick(60 *2)
+        self.dt =t/1000
+        
+    def deltaTime(self):
+        return self.dt
+    
+    
+    
+                
