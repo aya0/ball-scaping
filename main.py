1a6d7d0a7d5dd4853b7c2454478a057ff86e4c77
Aya <1220020@student.birzeit.edu>
2025-09-04 18:40:53 +0300

    Run the  blck screen
     nothing just the emply screen

--- main.py
+++ main.py
@@ -1,1 +1,23 @@
-print("check that everything work right ")
+import pygame
+
+from Game import Game
+from Utils import Utils
+
+game = Game()
+utils = Utils()
+
+while True:
+    utils.screen.fill((23,23,23) , (0 ,0 ,utils.width , utils.height))
+    utils.calDeltaTime()
+    
+    
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            exit(0)
+            
+    game.update()
+    game.draw()
+    
+    
+    pygame.display.flip()        
+
