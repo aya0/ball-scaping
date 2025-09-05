22f587da60d2a6c659f95f57f6e57b028b485f69
Aya <1220020@student.birzeit.edu>
2025-09-05 21:46:13 +0300

    add rings to the game

--- Game.py
+++ Game.py
@@ -1,17 +1,18 @@
 from pygame import Vector2
 
 from Ball import Ball
-from Utils import utils
+from Ring import Ring
+from utils import utils
 
-# add funcions to method
+
 class Game:
-    
     def __init__(self):
-        self.ball = Ball(Vector2(utils.width/2, utils.height/2) , 2 ,(255,255,255))
-    
+        self.ball = Ball(Vector2(utils.width/2,utils.height/2),2,(255,255,255))
+        self.ring = Ring(Vector2(utils.width/2,utils.height/2),20)
+
     def update(self):
-        utils.world.Step(1.0 / 60.0 , 6 ,2)
-    
+        utils.world.Step(1.0 / 60.0, 6, 2)
+
     def draw(self):
+        self.ring.draw()
         self.ball.draw()
-    
